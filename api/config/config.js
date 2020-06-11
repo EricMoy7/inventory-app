9b860ba4ae873f00d8ab34fa4d7ad56a86b9b62a
Floyd Anton Cruz <floyd.cruz@ymail.com>
2020-06-11 13:20:03 +0800

    initial commit for amazon wms endpoint

--- api/config/config.js
+++ api/config/config.js
@@ -7,6 +7,19 @@ module.exports = {
         user: "ericmoy77",
         password: "1295fr1295fr",
         database: "nodemysql"
+    },
+    "amazonWms": {
+        credentials: {
+            accessKey: "AKIAIPHR7XOXFJFBWQEQ",
+            secretKey: "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"
+        },
+        sellerId: "A350A0H0CAQ28C",
+        responseFormat: "JSON",
+        reportType: "_GET_FLAT_FILE_OPEN_LISTINGS_DATA_",
+        version: "2009-01-01",
+        requestReport: "RequestReport",
+        getReportRequestList: "GetReportRequestList",
+        getReport: "GetReport"
     }
 };
 
