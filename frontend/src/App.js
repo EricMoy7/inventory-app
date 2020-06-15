bb4092c91fe7332b9f9d18a600b90c1f94c6b175
Floyd Anton Cruz <floyd.cruz@ymail.com>
2020-06-15 22:32:15 +0800

    hotfix for folder name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 // App.js - Input editable UI
 
 import React, { useState } from "react";
-import Navigation from "./components/Navigation/Navigation";
+import Navigation from "./Components/Navigation/Navigation";
 
 function App() {
   // State for the input
