7f6895dd0ecfbd76999e81b9833126836496b283
Eric Moy <ericmoy@outlook.com>
2020-06-24 16:38:44 -0500

    Setup table for MBDtable, switching to material-table

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -3,40 +3,64 @@ import API from "../../utils/api";
 import config from "../../config/config";
 import { MDBDataTable } from "mdbreact";
 import { db } from "../Firebase";
+import { AddBox, ArrowDownward } from "@material-ui/icons";
 
 class Product extends Component {
   state = {
-    products: [],
+    products: {},
   };
 
   async componentDidMount() {
     const userData = JSON.parse(sessionStorage.getItem("userData"));
     const uid = userData.uid;
+    var colList = [];
+    var rowList = [];
 
     let user = db.collection("users").doc(uid).collection("MSKU");
-    let getDoc = user.get().then((snap) => {
-      const items = {};
-      snap.forEach((item) => {
-        items[item.id] = item.data();
-        return items;
+    user.orderBy("desc");
+    await user
+      .get()
+      .then((snap) => {
+        const items = {};
+        snap.forEach((item) => {
+          items[item.id] = item.data();
+          return items;
+        });
+        //Items is an object with MSKU as the key. The value of each key is the data of the key in another object
+        const firstMsku = Object.keys(items)[0];
+        const colObj = items[firstMsku];
+
+        Object.keys(colObj).forEach((key) => {
+          colList.push({
+            label: key.toUpperCase(),
+            field: key.toLowerCase(),
+            sort: "asc",
+            width: 150,
+          });
+        });
+
+        Object.keys(items).forEach((key) => {
+          rowList.push(items[key]);
+        });
+      })
+      .then(() => {
+        this.setState({ products: { columns: colList, rows: rowList } });
       });
-      //Items is an object with MSKU as the key. The value of each key is the data of the key in another object
-      console.log(items);
-    });
   }
 
   render() {
     const { products } = this.state;
-
     return (
-      <MDBDataTable
-        bordered
-        small
-        noBottomColumns
-        hover
-        info={false}
-        data={products}
-      />
+      <div>
+        <MDBDataTable
+          bordered
+          small
+          noBottomColumns
+          hover
+          info={false}
+          data={products}
+        />
+      </div>
     );
   }
 }
