5e2485573c04faba7d29c4eb049916b56900c898
Eric Moy <ericmoy@outlook.com>
2020-06-24 01:46:48 -0500

    returned JSON as object from database

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,40 +1,44 @@
-import React, { Component } from 'react';
-import API from '../../utils/api';
-import config from '../../config/config';
-import { MDBDataTable } from 'mdbreact';
+import React, { Component } from "react";
+import API from "../../utils/api";
+import config from "../../config/config";
+import { MDBDataTable } from "mdbreact";
+import { db } from "../Firebase";
 
 class Product extends Component {
-    state = {
-        products: []
-    }
-      
-    async componentDidMount() {
-        const { data } = await API.get('/product/data');
-        
-        if (data) {
-            this.setState({ 
-                products: {
-                    columns: config.columns,
-                    rows: data.data
-                }
-            });
-        }
-    }
+  state = {
+    products: [],
+  };
 
-    render() {
-        const { products } = this.state;
+  async componentDidMount() {
+    const userData = JSON.parse(sessionStorage.getItem("userData"));
+    const uid = userData.uid;
 
-        return (
-            <MDBDataTable
-                bordered
-                small
-                noBottomColumns
-                hover
-                info={false}
-                data={products}
-            />
-        );
-    }
+    let user = db.collection("users").doc(uid).collection("MSKU");
+    let getDoc = user.get().then((snap) => {
+      const items = {};
+      snap.forEach((item) => {
+        items[item.id] = item.data();
+        return items;
+      });
+      //Items is an object with MSKU as the key. The value of each key is the data of the key in another object
+      console.log(items);
+    });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <MDBDataTable
+        bordered
+        small
+        noBottomColumns
+        hover
+        info={false}
+        data={products}
+      />
+    );
+  }
 }
 
 export default Product;
