91c99ccd1f4fd798958a8c3833744c6ede2bcce4
Eric Moy <ericmoy@outlook.com>
2020-09-24 00:29:31 -0500

    Added styles utilities page for products page

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -3,49 +3,11 @@ import { db } from "../Firebase";
 import Axios from "axios";
 import MaterialTable from "material-table";
 import Container from "@material-ui/core/Container";
+import { renderTableStyle } from "./Utilities/RenderingStyles";
 
 const uid = JSON.parse(sessionStorage.getItem("userData")).uid;
 const inventory = db.collection(`users/${uid}/MSKU`);
 const headers = db.doc(`users/${uid}/settings/tableHeaders`);
-
-function renderTableStyle(colList) {
-  colList.map((column, idx) => {
-    if (column.title === "Image") {
-      colList[idx] = {
-        ...column,
-        render: (rowData) => (
-          <a href={`https://www.amazon.com/dp/${rowData.ASIN}`} target="_blank">
-            <img
-              src={rowData.imageUrl}
-              style={{
-                width: rowData.imageWidth,
-                height: rowData.imageHeight,
-              }}
-              alt={rowData["Product Name"]}
-            />
-          </a>
-        ),
-      };
-    }
-
-    if (column.title === "Availible Units") {
-      colList[idx] = {
-        ...column,
-        render: (rowData) =>
-          rowData["Availible Units"] === 0 ? (
-            <tr style={{ color: "red", position: "relative" }}>
-              {rowData["Availible Units"]}
-            </tr>
-          ) : (
-            <tr style={{ color: "black", position: "relative" }}>
-              {rowData["Availible Units"]}
-            </tr>
-          ),
-      };
-    }
-  });
-  return colList;
-}
 
 const Product = () => {
   const [rows, setRows] = useState([]);
@@ -76,9 +38,3 @@ const Product = () => {
 };
 
 export default Product;
-
-/*
-const uid = JSON.parse(sessionStorage.getItem("userData")).uid;
-  const inventory = db.collection(`users/${uid}/MSKU`);
-  const headers = db.doc(`users/${uid}/settings/tableHeaders`);
-  */
---
+++ src/Components/Products/Utilities/RenderingStyles.js
@@ -0,0 +1,38 @@
+export function renderTableStyle(colList) {
+  colList.map((column, idx) => {
+    if (column.title === "Image") {
+      colList[idx] = {
+        ...column,
+        render: (rowData) => (
+          <a href={`https://www.amazon.com/dp/${rowData.ASIN}`} target="_blank">
+            <img
+              src={rowData.imageUrl}
+              style={{
+                width: rowData.imageWidth,
+                height: rowData.imageHeight,
+              }}
+              alt={rowData["Product Name"]}
+            />
+          </a>
+        ),
+      };
+    }
+
+    if (column.title === "Availible Units") {
+      colList[idx] = {
+        ...column,
+        render: (rowData) =>
+          rowData["Availible Units"] === 0 ? (
+            <tr style={{ color: "red", position: "relative" }}>
+              {rowData["Availible Units"]}
+            </tr>
+          ) : (
+            <tr style={{ color: "black", position: "relative" }}>
+              {rowData["Availible Units"]}
+            </tr>
+          ),
+      };
+    }
+  });
+  return colList;
+}
